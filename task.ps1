f65d7dd8d6cc44c1d3933ee4424d162b766751c1
Illia Bilous <krabbys1111@gmail.com>
2025-08-12 00:07:46 +0300

    Enhance VM creation script in task.ps1 with additional resources and improved SSH key handling

--- task.ps1
+++ task.ps1
@@ -11,42 +11,84 @@ $sshKeyPublicKey = Get-Content "~/.ssh/id_rsa.pub" -Raw
 $vmName = "matebox"
 $vmImage = "Ubuntu2204"
 $vmSize = "Standard_B1s"
-$vmUser = "azureuser"  # заміни на бажане ім'я користувача
+$vmUser = "azureuser"
 
-Write-Host "Creating a resource group $resourceGroupName ..."
-New-AzResourceGroup -Name $resourceGroupName -Location $location
+# Resource Group
+if (-not (Get-AzResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue)) {
+    Write-Host "Creating resource group $resourceGroupName ..."
+    New-AzResourceGroup -Name $resourceGroupName -Location $location
+} else {
+    Write-Host "Resource group $resourceGroupName already exists."
+}
 
-Write-Host "Creating a network security group $networkSecurityGroupName ..."
-$nsgRuleSSH = New-AzNetworkSecurityRuleConfig -Name SSH -Protocol Tcp -Direction Inbound -Priority 1001 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 22 -Access Allow
-$nsgRuleHTTP = New-AzNetworkSecurityRuleConfig -Name HTTP -Protocol Tcp -Direction Inbound -Priority 1002 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 8080 -Access Allow
-New-AzNetworkSecurityGroup -Name $networkSecurityGroupName -ResourceGroupName $resourceGroupName -Location $location -SecurityRules $nsgRuleSSH, $nsgRuleHTTP
+# NSG
+$nsg = Get-AzNetworkSecurityGroup -Name $networkSecurityGroupName -ResourceGroupName $resourceGroupName -ErrorAction SilentlyContinue
+if (-not $nsg) {
+    Write-Host "Creating network security group $networkSecurityGroupName ..."
+    $nsgRuleSSH = New-AzNetworkSecurityRuleConfig -Name SSH -Protocol Tcp -Direction Inbound -Priority 1001 `
+        -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 22 -Access Allow
+    $nsgRuleHTTP = New-AzNetworkSecurityRuleConfig -Name HTTP -Protocol Tcp -Direction Inbound -Priority 1002 `
+        -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 8080 -Access Allow
+    $nsg = New-AzNetworkSecurityGroup -Name $networkSecurityGroupName -ResourceGroupName $resourceGroupName `
+        -Location $location -SecurityRules $nsgRuleSSH, $nsgRuleHTTP
+} else {
+    Write-Host "Network security group $networkSecurityGroupName already exists."
+}
 
-Write-Host "Creating virtual network $virtualNetworkName and subnet $subnetName ..."
-$subnetConfig = New-AzVirtualNetworkSubnetConfig -Name $subnetName -AddressPrefix $subnetAddressPrefix
-New-AzVirtualNetwork -Name $virtualNetworkName -ResourceGroupName $resourceGroupName -Location $location -AddressPrefix $vnetAddressPrefix -Subnet $subnetConfig
+# Virtual Network
+$vnet = Get-AzVirtualNetwork -Name $virtualNetworkName -ResourceGroupName $resourceGroupName -ErrorAction SilentlyContinue
+if (-not $vnet) {
+    Write-Host "Creating virtual network $virtualNetworkName and subnet $subnetName ..."
+    $subnetConfig = New-AzVirtualNetworkSubnetConfig -Name $subnetName -AddressPrefix $subnetAddressPrefix -NetworkSecurityGroup $nsg
+    $vnet = New-AzVirtualNetwork -Name $virtualNetworkName -ResourceGroupName $resourceGroupName -Location $location `
+        -AddressPrefix $vnetAddressPrefix -Subnet $subnetConfig
+} else {
+    Write-Host "Virtual network $virtualNetworkName already exists."
+}
 
-$dnsLabel = "$vmName-dns-$((Get-Random) -as [string])"
-Write-Host "Creating public IP address $publicIpAddressName with DNS label $dnsLabel ..."
-New-AzPublicIpAddress -Name $publicIpAddressName -ResourceGroupName $resourceGroupName -Location $location -AllocationMethod Static -DomainNameLabel $dnsLabel
+# Public IP
+$publicIp = Get-AzPublicIpAddress -Name $publicIpAddressName -ResourceGroupName $resourceGroupName -ErrorAction SilentlyContinue
+if (-not $publicIp) {
+    $dnsLabel = "$vmName-dns-$((Get-Random) -as [string])"
+    Write-Host "Creating public IP address $publicIpAddressName with DNS label $dnsLabel ..."
+    $publicIp = New-AzPublicIpAddress -Name $publicIpAddressName -ResourceGroupName $resourceGroupName -Location $location `
+        -AllocationMethod Static -DomainNameLabel $dnsLabel
+} else {
+    Write-Host "Public IP $publicIpAddressName already exists."
+}
 
-Write-Host "Creating SSH public key $sshKeyName ..."
-New-AzSshPublicKey -Name $sshKeyName -ResourceGroupName $resourceGroupName -Location $location -PublicKey $sshKeyPublicKey
+# SSH Key
+$sshKey = Get-AzSshPublicKey -Name $sshKeyName -ResourceGroupName $resourceGroupName -ErrorAction SilentlyContinue
+if (-not $sshKey) {
+    Write-Host "Creating SSH public key $sshKeyName ..."
+    $sshKey = New-AzSshPublicKey -Name $sshKeyName -ResourceGroupName $resourceGroupName -Location $location -PublicKey $sshKeyPublicKey
+} else {
+    Write-Host "SSH public key $sshKeyName already exists."
+}
 
-# Отримуємо ресурси
-$vnet = Get-AzVirtualNetwork -Name $virtualNetworkName -ResourceGroupName $resourceGroupName
-$subnet = $vnet.Subnets | Where-Object { $_.Name -eq $subnetName }
-$nsg = Get-AzNetworkSecurityGroup -Name $networkSecurityGroupName -ResourceGroupName $resourceGroupName
-$publicIp = Get-AzPublicIpAddress -Name $publicIpAddressName -ResourceGroupName $resourceGroupName
-$sshKey = Get-AzSshPublicKey -Name $sshKeyName -ResourceGroupName $resourceGroupName
+# NIC
+$nicName = "${vmName}Nic"
+$nic = Get-AzNetworkInterface -Name $nicName -ResourceGroupName $resourceGroupName -ErrorAction SilentlyContinue
+if (-not $nic) {
+    Write-Host "Creating network interface $nicName ..."
+    $subnet = $vnet.Subnets | Where-Object { $_.Name -eq $subnetName }
+    $nic = New-AzNetworkInterface -Name $nicName -ResourceGroupName $resourceGroupName -Location $location `
+        -SubnetId $subnet.Id -NetworkSecurityGroupId $nsg.Id -PublicIpAddressId $publicIp.Id
+} else {
+    Write-Host "Network interface $nicName already exists."
+}
 
-Write-Host "Creating network interface mateboxNic ..."
-$nic = New-AzNetworkInterface -Name "mateboxNic" -ResourceGroupName $resourceGroupName -Location $location `
-    -SubnetId $subnet.Id -NetworkSecurityGroupId $nsg.Id -PublicIpAddressId $publicIp.Id
+# VM
+$vm = Get-AzVm -Name $vmName -ResourceGroupName $resourceGroupName -ErrorAction SilentlyContinue
+if (-not $vm) {
+    Write-Host "Creating virtual machine $vmName ..."
+    $vmConfig = New-AzVmConfig -VMName $vmName -VMSize $vmSize | `
+        Set-AzVMOperatingSystem -Linux -ComputerName $vmName -Credential (New-Object System.Management.Automation.PSCredential ($vmUser, (ConvertTo-SecureString "dummyPassword123!" -AsPlainText -Force))) -DisablePasswordAuthentication | `
+        Set-AzVMSourceImage -PublisherName Canonical -Offer UbuntuServer -Skus 22_04-lts -Version latest | `
+        Add-AzVMNetworkInterface -Id $nic.Id | `
+        Set-AzVMSshPublicKey -KeyData $sshKeyPublicKey -Path "/home/$vmUser/.ssh/authorized_keys"
 
-Write-Host "Creating virtual machine $vmName ..."
-New-AzVm -ResourceGroupName $resourceGroupName -Name $vmName -Location $location `
-    -VirtualNetworkName $virtualNetworkName -SubnetName $subnetName -NetworkInterfaceName $nic.Name `
-    -PublicIpAddressName $publicIpAddressName -NetworkSecurityGroupName $networkSecurityGroupName `
-    -SshKeyName $sshKeyName -Image $vmImage -Size $vmSize -Credential (Get-Credential -UserName $vmUser -Message "Enter password (won't be used, SSH key auth)")
-
-Write-Host "VM creation script finished."
+    New-AzVm -ResourceGroupName $resourceGroupName -Location $location -VM $vmConfig
+} else {
+    Write-Host "Virtual machine $vmName already exists."
+}
