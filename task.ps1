7b586c38bde36d8591ac47da60832aaf44bd7d3f
Eldar <eldardemchenko19@mail.com>
2025-11-28 21:07:42 +0200

    Solution

--- task.ps1
+++ task.ps1
@@ -1,3 +1,4 @@
+# зміні для конфігурації віртуальнорї машини
 $location = "uksouth"
 $resourceGroupName = "mate-azure-task-9"
 $networkSecurityGroupName = "defaultnsg"
@@ -7,10 +8,13 @@ $vnetAddressPrefix = "10.0.0.0/16"
 $subnetAddressPrefix = "10.0.0.0/24"
 $publicIpAddressName = "linuxboxpip"
 $sshKeyName = "linuxboxsshkey"
-$sshKeyPublicKey = Get-Content "~/.ssh/id_rsa.pub" 
+$sshKeyPath = "C:/users/eldar/ssh/id_rsa.pub"
+$sshKeyPublicKey = $null
 $vmName = "matebox"
 $vmImage = "Ubuntu2204"
 $vmSize = "Standard_B1s"
+$dnsPrefix = "matebox$(Get-Random -Maximum 9999)"
+$adminUsername = "mateadmin"
 
 Write-Host "Creating a resource group $resourceGroupName ..."
 New-AzResourceGroup -Name $resourceGroupName -Location $location
@@ -20,4 +24,34 @@ $nsgRuleSSH = New-AzNetworkSecurityRuleConfig -Name SSH  -Protocol Tcp -Directio
 $nsgRuleHTTP = New-AzNetworkSecurityRuleConfig -Name HTTP  -Protocol Tcp -Direction Inbound -Priority 1002 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 8080 -Access Allow;
 New-AzNetworkSecurityGroup -Name $networkSecurityGroupName -ResourceGroupName $resourceGroupName -Location $location -SecurityRules $nsgRuleSSH, $nsgRuleHTTP
 
-# ↓↓↓ Write your code here ↓↓↓
+$subnet = New-AzVirtualNetworkSubnetConfig -Name $subnetName -AddressPrefix $subnetAddressPrefix -NetworkSecurityGroup (Get-AzNetworkSecurityGroup -Name $networkSecurityGroupName -ResourceGroupName $resourceGroupName)
+$Vnet = New-AzVirtualnetwork -Name $virtualNetworkName -ResourceGroupName $resourceGroupName -Location $location -AddressPrefix $vnetAddressPrefix -Subnet $subnet
+$publicIp = New-AzPublicIpAddress -Name $publicIpAddressName -ResourceGroupName $resourceGroupName -Location $location -AllocationMethod Static -DomainNameLabel $dnsPrefix
+
+if(Test-Path -PAth $sshKeyPath) {
+    Write-Host "SSH publik key found at $sshKeyPath"
+    $sshKeyPublicKey = Get-Content -Path $sshKeyPath -Raw
+} else {
+    Write-Host "SSH public key not found at $sshKeyPath. Please create SSH keys and try again."
+    $sshKeyPublicKey = $null
+}
+
+Write-Host "Creating SSH key resource $sshKeyName ..."
+if($sshKeyPublicKey) {
+    New-AzSshKey -ResourceGroupName $resourceGroupName -Name $sshKeyName -PublicKey $sshKeyPublicKey -Location $location
+} else {
+    New-AzSshKey -ResourceGroupName $resourceGroupName -Name $sshKeyName
+}
+
+New-AzVm `
+  -ResourceGroupName $resourceGroupName `
+  -Location $location `
+  -Name $vmName `
+  -Image $vmImage `
+  -Size $vmSize `
+  -VirtualNetworkName $virtualNetworkName `
+  -SubnetName $subnetName `
+  -SecurityGroupName $networkSecurityGroupName `
+  -PublicIpAddressName $publicIpAddressName `
+  -SshKeyName $sshKeyName `
+
