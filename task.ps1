a62b7d0739fb208a707f09450615ff58ed583d74
Anastasiia <anastasiia.chikrizova@dashdevs.com>
2026-07-30 15:43:01 +0300

    Update script to deploy Azure resources, switch region to "denmarkeast", and add resource template link in artifacts.

--- task.ps1
+++ task.ps1
@@ -1,4 +1,4 @@
-$location = "uksouth"
+$location = "denmarkeast"
 $resourceGroupName = "mate-azure-task-9"
 $networkSecurityGroupName = "defaultnsg"
 $virtualNetworkName = "vnet"
@@ -20,4 +20,41 @@ $nsgRuleSSH = New-AzNetworkSecurityRuleConfig -Name SSH  -Protocol Tcp -Directio
 $nsgRuleHTTP = New-AzNetworkSecurityRuleConfig -Name HTTP  -Protocol Tcp -Direction Inbound -Priority 1002 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 8080 -Access Allow;
 New-AzNetworkSecurityGroup -Name $networkSecurityGroupName -ResourceGroupName $resourceGroupName -Location $location -SecurityRules $nsgRuleSSH, $nsgRuleHTTP
 
-# ↓↓↓ Write your code here ↓↓↓
+Write-Host "Creating a virtual network $virtualNetworkName ..."
+$subnet = New-AzVirtualNetworkSubnetConfig `
+  -Name $subnetName `
+  -AddressPrefix $subnetAddressPrefix
+New-AzVirtualNetwork `
+  -Name $virtualNetworkName `
+  -ResourceGroupName $resourceGroupName `
+  -Location $location `
+  -AddressPrefix $vnetAddressPrefix `
+  -Subnet $subnet
+
+Write-Host "Creating a public IP address $publicIpAddressName ..."
+New-AzPublicIpAddress `
+  -Name $publicIpAddressName `
+  -ResourceGroupName $resourceGroupName `
+  -Location $location `
+  -Sku Standard `
+  -AllocationMethod Static `
+  -DomainNameLabel "matebox-$(Get-Random)"
+
+Write-Host "Creating an SSH key resource $sshKeyName ..."
+New-AzSshKey `
+  -Name $sshKeyName `
+  -ResourceGroupName $resourceGroupName `
+  -PublicKey $sshKeyPublicKey
+
+Write-Host "Creating a virtual machine $vmName ..."
+New-AzVm `
+  -ResourceGroupName $resourceGroupName `
+  -Name $vmName `
+  -Location $location `
+  -Image $vmImage `
+  -Size $vmSize `
+  -VirtualNetworkName $virtualNetworkName `
+  -SubnetName $subnetName `
+  -PublicIpAddressName $publicIpAddressName `
+  -SecurityGroupName $networkSecurityGroupName `
+  -SshKeyName $sshKeyName
