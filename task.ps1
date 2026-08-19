6f2b8823ed07ff08741450aba337d287087b066d
Vitalii <stalchenkovitaliy@gmail.com>
2026-08-19 12:22:43 +0400

    create vm with powershell

--- task.ps1
+++ task.ps1
@@ -1,4 +1,4 @@
-$location = "uksouth"
+$location = "polandcentral"
 $resourceGroupName = "mate-azure-task-9"
 $networkSecurityGroupName = "defaultnsg"
 $virtualNetworkName = "vnet"
@@ -7,10 +7,10 @@ $vnetAddressPrefix = "10.0.0.0/16"
 $subnetAddressPrefix = "10.0.0.0/24"
 $publicIpAddressName = "linuxboxpip"
 $sshKeyName = "linuxboxsshkey"
-$sshKeyPublicKey = Get-Content "~/.ssh/id_rsa.pub" 
+$sshKeyPublicKey = Get-Content "~/.ssh/id_rsa.pub"
 $vmName = "matebox"
 $vmImage = "Ubuntu2204"
-$vmSize = "Standard_B1s"
+$vmSize =  "Standard_B2ats_v2"
 
 Write-Host "Creating a resource group $resourceGroupName ..."
 New-AzResourceGroup -Name $resourceGroupName -Location $location
@@ -21,3 +21,58 @@ $nsgRuleHTTP = New-AzNetworkSecurityRuleConfig -Name HTTP  -Protocol Tcp -Direct
 New-AzNetworkSecurityGroup -Name $networkSecurityGroupName -ResourceGroupName $resourceGroupName -Location $location -SecurityRules $nsgRuleSSH, $nsgRuleHTTP
 
 # ↓↓↓ Write your code here ↓↓↓
+Write-Host "Creating subnet $subnetName ..."
+
+$subnet = New-AzVirtualNetworkSubnetConfig `
+    -Name $subnetName `
+    -AddressPrefix $subnetAddressPrefix
+
+
+Write-Host "Creating virtual network $virtualNetworkName ..."
+
+New-AzVirtualNetwork `
+    -Name $virtualNetworkName `
+    -ResourceGroupName $resourceGroupName `
+    -Location $location `
+    -AddressPrefix $vnetAddressPrefix `
+    -Subnet $subnet
+
+
+Write-Host "Creating public IP $publicIpAddressName ..."
+
+New-AzPublicIpAddress `
+    -Name $publicIpAddressName `
+    -ResourceGroupName $resourceGroupName `
+    -Location $location `
+    -AllocationMethod Static `
+    -Sku Standard `
+    -DomainNameLabel "matebox-$((Get-Random -Maximum 99999))"
+
+
+Write-Host "Creating SSH key resource $sshKeyName ..."
+
+New-AzSshKey `
+    -ResourceGroupName $resourceGroupName `
+    -Name $sshKeyName `
+    -PublicKey $sshKeyPublicKey
+
+
+Write-Host "Creating VM $vmName ..."
+
+$credential = Get-Credential -UserName "azureuser" `
+    -Message "Enter a password for the VM user (SSH key will be used for login)"
+
+New-AzVM `
+    -ResourceGroupName $resourceGroupName `
+    -Location $location `
+    -Name $vmName `
+    -Credential $credential `
+    -VirtualNetworkName $virtualNetworkName `
+    -SubnetName $subnetName `
+    -PublicIpAddressName $publicIpAddressName `
+    -SecurityGroupName $networkSecurityGroupName `
+    -SshKeyName $sshKeyName `
+    -Image $vmImage `
+    -Size $vmSize
+
+Write-Host "Done!"
